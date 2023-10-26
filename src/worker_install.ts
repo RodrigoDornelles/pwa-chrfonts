667b27b4dc03491350a47468ea1e45b39a3d2933
rodrigodornelles <rodrigo@dornelles.me>
2023-10-26 13:07:23 -0300

    fix: format

--- src/worker_install.ts
+++ src/worker_install.ts
@@ -1,11 +1,12 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('worker_service.js')
-        .then((registration) => {
-            console.log('Service Worker registered with scope:', registration.scope)
-        })
-        .catch((error) => {
-            console.warn('Service Worker registration failed:', error)
-        })
+        navigator.serviceWorker
+            .register('worker_service.js')
+            .then((registration) => {
+                console.log('Service Worker registered with scope:', registration.scope)
+            })
+            .catch((error) => {
+                console.warn('Service Worker registration failed:', error)
+            })
     })
 }
